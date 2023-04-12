d0bbe8c17af097fa3a080681fa5b46cfd6bf6cd8
Mishael <mishaelclifford1@gmail.com>
2023-04-12 13:54:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Body from "./components/Body";
+
+function App(props) {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: "Mish", age: 20 },
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+    ],
+    otherState: "some other value",
+  });
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: "Big-Mish", age: 19 },
+        { name: "Max", age: 30 },
+        { name: "Manu", age: 24 },
+      ],
+    });
+  };
+  console.log(personsState);
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl font-bold">Hi,I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>switch Name</button>
+      <Body name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Body name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Body name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Render.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Render() {
+  return (
+    <div className="">
+     
+    </div>
+  );
+}
+
+export default Render;
